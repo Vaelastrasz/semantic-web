ac80655e7253d6de454d733cfe5555ce76d637ec
Vlad Chobanitsa <Chobanitsavlad@gmail.com>
2022-06-02 01:40:04 +0300

    lab3 swagger add

--- semantksite/semantksite/settings.py
+++ semantksite/semantksite/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Social.apps.SocialConfig',
-    'rest_framework'
+    'rest_framework',
+    'drf_yasg'
 ]
 
 MIDDLEWARE = [
